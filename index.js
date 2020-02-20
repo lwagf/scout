2d2adf540357019b5d11c44f823b0e8e3067d04f
Logan White <git@loganwhite.org>
2020-02-20 21:18:10 +0000

    Begin config and index

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ *  Scout - a Yu-Gi-Oh! discord bot
+ *
+ *  The bot has 2 main functions:
+ *   -  Fetch news articles every hour
+ *   -  Provide a card text search
+ */
+
+//a. News articles
+
+//b. Card search
