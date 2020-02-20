f2ae6f4991a6e507e9f9817fd09eb457407f4b07
Logan White <git@loganwhite.org>
2020-02-20 23:37:51 +0000

    latestUrl as a local let isnt going to work

--- schedule.js
+++ schedule.js
@@ -1,9 +1,9 @@
 const latestTweets = require('util').promisify(require('latest-tweets'));
 const nodeSchedule = require('node-schedule');
 
+module.latestUrl = null;
 module.exports = {
     start : (channelNews) => {
-        let latestUrl;
         nodeSchedule.scheduleJob('*/5 * * * *', async () => {
             let tweets;
             try {
@@ -12,13 +12,13 @@ module.exports = {
                 return console.warn(`** Failed to retrieve tweets: ${error.toString()}`);
             }
             const tweetFirst = tweets[0];
-            if (tweetFirst.url != latestUrl) {
+            if (tweetFirst.url != module.latestUrl) {
                 const content = tweetFirst.content;
                 //Filter out the irrelevant tweets
                 if (content.includes("[Today's Yu-Gi-Oh! OCG Card Intro") || content.includes("[Quiz for Everyone]")) {
                     return;
                 }
-                latestUrl = tweetFirst.url;
+                module.latestUrl = tweetFirst.url;
                 //Post to discord channel
                 return channelNews.send(content);
             }
