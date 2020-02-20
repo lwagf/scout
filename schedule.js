c7dcf459449b7d82365ea786282bbb99f937059d
Logan White <git@loganwhite.org>
2020-02-20 21:46:12 +0000

    Init the news schedule module

---
+++ schedule.js
@@ -0,0 +1,27 @@
+const latestTweets = require('latest-tweets');
+const nodeSchedule = require('node-schedule');
+
+module.exports = {
+    start : (channelNews) => {
+        let latestUrl;
+        nodeSchedule.scheduleJob('*/5 * * * *', async () => {
+            let tweets;
+            try {
+                tweets = await latestTweets('ygorganization');
+            } catch (error) {
+                return console.warn(`** Failed to retrieve tweets: ${error.toString()}`);
+            }
+            const tweetFirst = tweets[0];
+            if (tweetFirst.url != latestUrl){
+                const content = tweetFirst.content;
+                //Filter out the irrelevant tweets
+                if (content.includes("[Today's Yu-Gi-Oh! OCG Card Intro") || content.includes("[Quiz for Everyone]")){
+                    return;
+                }
+                latestUrl = tweetFirst.url;
+                //Post to discord channel
+                return channelNews.send(content);
+            }
+        });
+    }
+};
