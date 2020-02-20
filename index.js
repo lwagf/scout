fdb40c6b91a8144ce416c9329f3a028cfadbd0dd
Logan White <git@loganwhite.org>
2020-02-20 22:54:41 +0000

    Initial index and search implementations

--- index.js
+++ index.js
@@ -2,10 +2,47 @@
  *  Scout - a Yu-Gi-Oh! discord bot
  *
  *  The bot has 2 main functions:
- *   -  Fetch news articles every 5th minute
- *   -  Provide a card text search
+ *   -  Fetch news articles every 5th minute - schedule.js
+ *   -  Provide a card text search - search.js
  */
 
-//a. News articles
+const _packageJson = require('./package.json');
+console.log(`Starting Scout ${_packageJson.version}`);
+const _config = require('./config.json');
+const botToken = _config.botToken;
+if (!botToken) {
+    return console.exception('No bot token in config - not continuing');
+}
+if (!_config.channelNews) {
+    console.warn('** No channel specified for posting news **');
+}
+const discord = require('discord.js');
+const Client = new discord.Client();
+const moduleSchedule = require('./schedule.js');
+const moduleSearch = require('./search.js');
+const regexSearch = /scout!(.+)/;
 
-//b. Card search
+Client.on('message', async (msg) => {
+    const cardTitle = (msg.content.match(regexSearch) || [])[1];
+    if (cardTitle && !msg.author.bot){
+        const channelSent = msg.channel;
+        return moduleSearch(channelSent, cardTitle).catch((error) => {
+            console.warn(`** Couldn't return data for '${cardTitle}' (${error.toString()}) **`);
+            channelSent.send('An error occured');
+        });
+    }
+});
+
+Client.on('ready', async () => {
+    const countServers = Client.guilds.size;
+    const user = Client.user;
+    console.info(`Logged in as ${user.tag}!`);
+    user.setActivity(`Serving YGO data on ${countServers} servers`);
+    moduleSchedule.start();
+});
+
+async function main() {
+    Client.login(botToken);
+}
+
+return main().catch((error) => console.exception(error));
---
+++ search.js
@@ -0,0 +1,96 @@
+const axios = require('axios');
+module.exports = async (channelSent, cardTitle) => {
+    const response = await axios({
+        method : 'get',
+        url : 'https://db.ygoprodeck.com/api/v6/cardinfo.php',
+        params : {
+            // We'll use the fuzzy search & take the top result
+            fname : cardTitle
+        },
+        // Some non 200/300's are ok - see below
+        validateStatus : undefined
+    });
+    if (response.status === 400) {
+        //This is usually just that the user's card wasn't found - would be silly to log all of these
+        return channelSent.send(`Card '${cardTitle}' not found`);
+    } else if (response.status !== 200) {
+        //This is assuming we don't ever receive redirects
+        throw `Non ok status ${response.status} - ${JSON.stringify(response.data)}`;
+    }
+    const cardFirst = response.data[0];
+    if (!cardFirst){
+        //This should never happen!
+        throw `No cardFirst for '${cardTitle}'`;
+    }
+
+    const fieldsEmbed = [
+        {
+            name : 'classification',
+            value : cardFirst.type,
+            inline : true
+        },
+        {
+            name : 'type',
+            value : cardFirst.race,
+            inline : true
+        },
+        {
+            name : 'archetype',
+            value : cardFirst.archetype || 'none',
+            inline : true
+        }
+    ];
+    if (cardFirst.level) {
+        fieldsEmbed.push(
+            {
+                name : 'level / rank',
+                value: cardFirst.level,
+                inline : true
+            }
+        );
+    }
+    if (cardFirst.linkval) {
+        fieldsEmbed.push(
+            {
+                name : 'link rating',
+                value: cardFirst.linkval,
+                inline : true
+            }
+        );
+    }
+    if (cardFirst.attribute) {
+        //Its a monster
+        fieldsEmbed.push(
+            {
+                name : 'attribute',
+                value: cardFirst.attribute,
+                inline : true
+            },
+            {
+                name : 'atk',
+                value: cardFirst.atk,
+                inline : true
+            },
+            {
+                name : 'def',
+                value: cardFirst.def,
+                inline : true
+            }
+        );
+    }
+
+    return channelSent.send({
+        embed : {
+            color : 0xFCBA03, //a yellow to match qli scout :)
+            image : {
+                url : (cardFirst.card_images[0] || {}).image_url
+            },
+            title : cardFirst.name,
+            description : cardFirst.desc,
+            fields: fieldsEmbed,
+            footer : {
+                text : `${cardFirst.id} | made by logan`
+            }
+        }
+    });
+};
